b51b3296ff95fae3c05ec009df70309adb7f07f8
minyoung <alsdud11737@gmail.com>
2023-05-14 21:27:36 +0900

    bonus assignment

--- src/com/day_0513/entity/Bonus.java
+++ src/com/day_0513/entity/Bonus.java
@@ -1,5 +1,68 @@
 package com.day_0513.entity;
 
 public class Bonus {
+	private String ename;
+    private String job;
+    private String sal;
+    private String comm;
+    
+    
+    //생성자
+	public Bonus(String ename, String job, String sal, String comm) {
+		super();
+		this.ename = ename;
+		this.job = job;
+		this.sal = sal;
+		this.comm = comm;
+	}
+
+
+	public String getEname() {
+		return ename;
+	}
+
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+
+	public String getJob() {
+		return job;
+	}
+
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+
+	public String getSal() {
+		return sal;
+	}
+
+
+	public void setSal(String sal) {
+		this.sal = sal;
+	}
+
+
+	public String getComm() {
+		return comm;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Bonus [ename=" + ename + ", job=" + job + ", sal=" + sal + ", comm=" + comm + "]";
+	}
+
+
+	public void setComm(String comm) {
+		this.comm = comm;
+	}
+    
+	
+        
 
 }
--- src/com/day_0513/entity/Emp.java
+++ src/com/day_0513/entity/Emp.java
@@ -2,4 +2,62 @@ package com.day_0513.entity;
 
 public class Emp {
 
+	private int empno;
+	private String ename;
+	private String job;
+
+	public Emp(String ename, int empno, String job, String sal, String comm) {
+		super();
+		this.empno = empno;
+		this.ename = ename;
+		this.job = job;
+		this.sal = sal;
+		this.comm = comm;
+	}
+
+	private String sal;
+	private String comm;
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	// null 값을 0 으로 받아오기
+	public String getSal() {
+		return sal == null ? "0" : sal;
+	}
+
+	public void setSal(String sal) {
+		this.sal = sal;
+	}
+
+	// null 값을 0 으로 받아오기
+	public String getComm() {
+		return comm == null ? "0" : comm;
+	}
+
+	public void setComm(String comm) {
+		this.comm = comm;
+	}
+
 }
